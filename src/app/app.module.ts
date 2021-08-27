3d8b47072874a882b1926cde217eb7ce95e6698b
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-08-27 21:40:55 +0530

    Dashboard Card done in home and country wise total counts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,26 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { CountriesComponent } from './components/countries/countries.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    CountriesComponent
+    CountriesComponent,
+    DashboardCardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -7,30 +7,43 @@ import { GlobalDataSummary } from '../models/global-data';
   providedIn: 'root',
 })
 export class DataServiceService {
-  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-24-2021.csv`;
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/06-24-2021.csv`;
 
   constructor(private http: HttpClient) {}
 
   getGlobalData() {
     return this.http.get(this.globalDataUrl, { responseType: 'text' }).pipe(
       map((result) => {
-        let data: GlobalDataSummary[] = [];
+        //let data: GlobalDataSummary[] = [];
+        let raw = {}; // Intialising a dictionary which will hold Country as key and its values
         let rows = result.split('\n');
-        rows.splice(0,1)
+        rows.splice(0, 1);
 
         rows.forEach((row) => {
           let cols = row.split(/,(?=\S)/); // This will split in ',' and ignore ', ' which is a field value
-          data.push({
+          let cs = {
             country: cols[3],
             confirmed: +cols[7], // + operator converts string to integer
             deaths: +cols[8],
             recovered: +cols[9],
             active: +cols[10],
-          });
+          };
+
+          // Grouping by Countries (Adding different states values of same countries)
+          let temp: GlobalDataSummary = raw[cs.country]; //fetching current country value from raw if there is a match
+          if (temp) {           // If the country already added, then add the values in same country
+            temp.confirmed += cs.confirmed
+            temp.deaths += cs.deaths
+            temp.recovered += cs.recovered
+            temp.active += cs.active
+          } 
+          else { // If not added, add it into raw 
+            raw[cs.country] = cs
+          }
         });
 
-        console.log(data)
-        return [];
+        //console.log(raw);
+        return <GlobalDataSummary[]>Object.values(raw);
       })
     );
   }
